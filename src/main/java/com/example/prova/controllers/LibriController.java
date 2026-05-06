0e199227ea2c20eafb9aaa30bd9d4787e12a5845
DY00330 <andrea.scarrone@realeites.com>
2026-05-06 17:42:53 +0200

    connessione con frontend

--- src/main/java/com/example/prova/controllers/LibriController.java
+++ src/main/java/com/example/prova/controllers/LibriController.java
@@ -3,6 +3,7 @@ package com.example.prova.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import com.example.prova.services.LibroService;
 
 @RestController
 @RequestMapping("/api/libri")
+@CrossOrigin("http://localhost:4200/")
 public class LibriController {
     
     @Autowired
